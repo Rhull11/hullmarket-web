e51b52844de79f7f4cc26d8fc4d2ead9975da811
R Hull <rhull9@outlook.com>
2020-06-07 06:51:11 -0600

    Added About page, updated navbar.

--- about.php
+++ about.php
@@ -11,10 +11,9 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 	<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/navbar-static/">
 </head>
-
 <body>
 
- <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+	 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <a class="navbar-brand" href="index.php">Hullmarket.com<span class="sr-only">(current)</span></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -22,48 +21,15 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="about.php">About and Contact</a>
+            <a class="nav-link" href="#">About and Contact</a>
           </li>
         </ul>
-        <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          <a class="nav-link" href="#">Shopping Cart</a>
-        </form>
+        <div class="topnav-right">
+          <a class="nav-link" href="#">Shopping Cart</a>         
+        </div>
       </div>
     </nav>
-    
-    
-    <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Item</th>
-      <th scope="col">Description</th>
-      <th scope="col">Price</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+  
 
 </body>
 </html>
